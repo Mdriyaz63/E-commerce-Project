dd3f9c0a875e57acab5cc8da4953a15fcbf39772
Mdriyaz63 <143110452+Mdriyaz63@users.noreply.github.com>
2024-10-16 14:42:55 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,9 @@
+ let card = document.querySelectorAll(".trend");
+ let blog = document.querySelector(".trends");
+
+ let mainPage = document.querySelector(".main");
+ function shops(){
+    mainPage.style.display="none";
+    blog.style.display="none";
+    
+ }
